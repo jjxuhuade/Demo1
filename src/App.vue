849ab165b250a11a39baecbbe034e83225a53d77
jjxuhude <jjxuhuade@163.com>
2017-08-10 15:37:53 +0800

    upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <jjHeader>header</jjHeader>
+        <router-view></router-view>
+        <jjFooter>footer</jjFooter>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/style.css'
+import 'nprogress/nprogress.css'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import nprogress from 'nprogress'
+
+axios.defaults.baseURL = HOST
+axios.defaults.timeout = 1000 * 15
+axios.defaults.withCredentials = true
+axios.defaults.headers.session_id = lockr.get('session_id') ? lockr.get('session_id') : ''
+axios.defaults.headers['Content-Type'] = 'application/json'
+
+window.nprogress = nprogress
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {App}
+})
